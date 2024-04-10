71d9363c81bc638717fb91e5a6f9a193975266c0
Mahmoud Alwadia <mahmoud.alwadia@gmail.com>
2024-04-10 11:57:58 +0200

    Add redirects rule for posts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   env: {
     GA_MEASUREMENT_ID: process.env.GA_MEASUREMENT_ID,
   },
+  async redirects() {
+    return [
+      {
+        source: '/a-hurtful-advancing-civilization',
+        destination:
+          'https://open.substack.com/pub/mahmoudalwadia/p/a-hurtful-advancing-civilization',
+        permanent: true,
+      },
+      {
+        source: '/blog/make-mobility-fun-and-sustainable',
+        destination:
+          'https://github.com/mahmoudalwadia/mahmoudalwadia.com/blob/c945e6f5ad12a963e0a41f7d5ea2fe733f4018da/data/posts/make-mobility-fun-and-sustainable.mdx',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
